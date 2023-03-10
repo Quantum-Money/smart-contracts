9f44064238eeb9fc78b3a1f9e8fde61d756fc482
Quantum-Money <125833121+Quantum-Money@users.noreply.github.com>
2023-03-10 18:27:22 +0300

    Update and rename EUROQ.sol to EUREQ.sol

--- ERC20/EUREQ.sol
+++ ERC20/EUREQ.sol
@@ -4,8 +4,8 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract EUROQERC20 is ERC20, ERC20Burnable, Ownable {
-    constructor() ERC20("Quantum", "EUROQ") {}
+contract EUREQERC20 is ERC20, ERC20Burnable, Ownable {
+    constructor() ERC20("EQuicoin EUR", "EUREQ") {}
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
