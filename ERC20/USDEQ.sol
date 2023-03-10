8fc94c6fac5dac0a7cf5af4fe7b18bd1c92146cc
Quantum-Money <125833121+Quantum-Money@users.noreply.github.com>
2023-03-10 18:28:25 +0300

    Update and rename USDQC.sol to USDEQ.sol

--- ERC20/USDEQ.sol
+++ ERC20/USDEQ.sol
@@ -4,8 +4,8 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract USDQCERC20 is ERC20, ERC20Burnable, Ownable {
-    constructor() ERC20("Quantum", "USDQC") {}
+contract USDEQERC20 is ERC20, ERC20Burnable, Ownable {
+    constructor() ERC20("EQuicoin USD", "USDEQ") {}
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
